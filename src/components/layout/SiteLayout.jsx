a6a5e59e0eb5feb90cc5b3ba4d1902e6145b0bef
ustarenovasyon <ustarenovasyon@gmail.com>
2026-09-07 11:55:09 +0300

    Remove AI assistant and floating chat bubbles

--- src/components/layout/SiteLayout.jsx
+++ src/components/layout/SiteLayout.jsx
@@ -5,14 +5,12 @@ import TopInfoBar from "@/components/layout/TopInfoBar";
 import SiteFooter from "@/components/layout/SiteFooter";
 import MobileCommandBar from "@/components/layout/MobileCommandBar";
 import DesktopContactBar from "@/components/layout/DesktopContactBar";
-import WhatsAppFloat from "@/components/layout/WhatsAppFloat";
 import CookieConsentBanner from "@/components/common/CookieConsentBanner";
 import MaintenanceGate from "@/components/common/MaintenanceGate";
-import AssistantWidget from "@/components/assistant/AssistantWidget";
 import { SiteSeoDefaults } from "@/components/common/SeoHead";
 
 // Tüm site sayfalarını saran yerleşim: üst başlık + içerik + alt bilgi + sabit çubuklar.
-// SiteSeoDefaults: canonical, robots, OG/Twitter varsayılanları, global LocalBusiness.
+// Kullanıcı isteğiyle sağ taraftaki yapay zekâ asistanı ve yüzen sohbet balonları tamamen kaldırıldı.
 export default function SiteLayout() {
   return (
     <div className="flex min-h-screen flex-col">
@@ -27,8 +25,6 @@ export default function SiteLayout() {
       <SiteFooter />
       <MobileCommandBar />
       <DesktopContactBar />
-      <WhatsAppFloat />
-      <AssistantWidget />
       <CookieConsentBanner />
     </div>
   );
